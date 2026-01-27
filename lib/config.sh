378e6b1495c6926df29936f1c1d11dfb0546f8c6
Alexander Christian <alexanderechristian@gmail.com>
2026-01-27 13:30:44 -0500

    New script to handle configuration

---
+++ lib/config.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Strict settings
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# On-the-fly-debugging
+[[ -n "${DEBUG:-}" ]] && set -x
+
+# "Magic" variables
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename "${__file}" .sh)"
+
+# Borrowed from https://paul.af/bash-script-preamble-boilerplate
+
+
+
+#CONFIG_DIR="${XDG_CONFIG_HOME}/manifest"
+CONFIG_DIR=${__dir}/../config"
+
+[ ! -d "$CONFIG_DIR" ] && mkdir "$CONFIG_DIR"
