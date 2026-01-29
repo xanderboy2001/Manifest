428f47fcd1cfeab019cb57591d475d0f22ad5d03
Alexander Christian <alexanderechristian@gmail.com>
2026-01-29 16:03:09 -0500

    feat(stow): enhance add_config error handling and success feedback
    * Add logic to distinguish between error states and successful configuration imports
    * Implement `print_success` message upon successful addition of configuration files
    * Improve error reporting when a path cannot be added to the manifest
    * Refactor `handle_stow_menu` to use conditional output based on `StowManager.add_config` results

--- manifest/main.py
+++ manifest/main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from .lib.config import ConfigManager
 from .lib.stow import StowManager
 from .lib.ui import UIManager
-from .lib.utils import print_debug, print_error, print_menu_output
+from .lib.utils import print_debug, print_error, print_menu_output, print_success
 
 
 def handle_stow_menu(stowManager: StowManager, ui: UIManager) -> bool:
@@ -22,7 +22,12 @@ def handle_stow_menu(stowManager: StowManager, ui: UIManager) -> bool:
                     message="Path to configuration files (/home/user/.config/program)",
                     starting_dir="~",
                 )
-                print_menu_output(stowManager.add_config(Path(config_path)))
+                output = stowManager.add_config(Path(config_path))
+                if output == "error":
+                    print_error(f"Could not add {config_path}")
+                else:
+                    print_menu_output(output)
+                    print_success(f"Successfully added {config_path} to Manifest!")
             case "remove_config":
                 pass
             case "deploy_config":
